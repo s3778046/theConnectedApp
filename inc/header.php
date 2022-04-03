116112fd57f869f92c394f7edceff11cca4751a9
Mark <s3778046@student.rmit.edu.au>
2022-04-03 22:13:24 +1000

    PDO connection to database, changes to index.php and header.php

--- inc/header.php
+++ inc/header.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -55,3 +57,4 @@
             </div>
         </nav>   
     </header>
+    <?php include("tools.php"); ?>
--- index.php
+++ index.php
@@ -1,23 +1,33 @@
 <?php   include("./inc/header.php");  ?>
 
+<main>   
+    <?php
+        
+        try {
+            $stmt = $conn->prepare("SELECT * FROM users");
+            $stmt->execute();
 
-<main>
-    
+            // set the resulting array to associative
+            $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+            while ($row = $stmt->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
+                $data = $row[0] . "\t" . $row[1] . "\t" . $row[2] . "\n";
+                print $data;
+            }
+        
+            
+        
+            
+        }
+        catch(PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+        $conn = null;
+        echo "</table>";
+    ?>
 </main>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 <?php   include("./inc/footer.php");  ?>
+
+
+
